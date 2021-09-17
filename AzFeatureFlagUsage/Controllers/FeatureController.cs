1665a87c44e5eb94f6de8f5ccfc9374b01b0432f
hdurmus <hasan.durmus@tacirler.com.tr>
2021-09-17 10:07:27 +0300

    Add project files.

---
+++ AzFeatureFlagUsage/Controllers/FeatureController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzFeatureFlagUsage.Controllers
+{
+    public class FeatureController : Controller
+    {
+        private readonly IFeatureManager _featureManager;
+
+        public FeatureController(IFeatureManager featureManager)
+        {
+            _featureManager = featureManager;
+        }
+
+        [FeatureGate(FeatureFlags.Staging)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AzFeatureFlagUsage/FeatureFlags.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzFeatureFlagUsage
+{
+    public enum FeatureFlags
+    {
+        Staging
+    }
+}
